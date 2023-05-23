712c9aabe766e3c51e18c62a543b6e5978821bb6
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2023-05-23 17:43:15 -0400

    Use Craco and esbuild to speed up build and deploy time

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -22,7 +22,7 @@ export default function App() {
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Create React App example in TypeScript
+          AHH BEES!
         </Typography>
         <ProTip />
         <Copyright />
